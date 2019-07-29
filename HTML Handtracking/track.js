8487cfd2f8913894bc23867883256f50c8e55171
Anna <anna.y.dong@gmail.com>
2019-07-29 10:37:28 -0700

    scrolling attached to cursor

--- HTML Handtracking/track.js
+++ HTML Handtracking/track.js
@@ -48,59 +48,64 @@ function toggleVideo() {
 }
 
 function runDetection() {
-    model.detect(video).then(predictions => {
-        console.log("Predictions: ", predictions);
-        model.renderPredictions(predictions, canvas, context, video);
-        if (predictions[0]) {
-            let midvalX = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2)
-            gamex = moveCanvas.width * (midvalX / video.width)
-            console.log(midvalX);
-            console.log('Predictions: ', gamex);
-            let midvalY = predictions[0].bbox[1] + (predictions[0].bbox[3] / 2)
-            gamey = moveCanvas.height * (midvalY / video.height)
-            console.log(midvalY);
-            console.log('Predictions: ', gamey);
+  model.detect(video).then(predictions => {
+    console.log("Predictions: ", predictions);
+    model.renderPredictions(predictions, canvas, context, video);
+    if (predictions[0]) {
+      let midvalX = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2)
+      gamex = moveCanvas.width * (midvalX / video.width)
+      console.log(midvalX);
+      console.log('Predictions: ', gamex);
+      let midvalY = predictions[0].bbox[1] + (predictions[0].bbox[3] / 2)
+      gamey = moveCanvas.height * (midvalY / video.height)
+      console.log(midvalY);
+      console.log('Predictions: ', gamey);
 
-            let x = gamex;
-            let y = gamey;
+      let x = gamex;
+      let y = gamey;
 
-            draw();
+      draw();
+      function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI*2);
+        ctx.fillStyle = "rgba(255,51,51,0.5)";
+        ctx.fill();
+      }
+      function draw() {
+        ctx.clearRect(0, 0, moveCanvas.width, moveCanvas.height);
+        drawBall();
+      }
 
-            function drawBall() {
-                ctx.beginPath();
-                ctx.arc(x, y, 10, 0, Math.PI*2);
-                ctx.fillStyle = "rgba(255,51,51,0.5)";
-                ctx.fill();
-            }
-
-            function draw() {
-                ctx.clearRect(0, 0, moveCanvas.width, moveCanvas.height);
-                drawBall();
-
-            }
-
-            //setInterval(draw, 10);
-            //item.style.right = -x + "px";
-            //item.style.bottom = -y + "px";
-            //item.style.left = x + "px";
-            //item.style.top = y + "px";
-
-        }
-        if (isVideo) {
-            requestAnimationFrame(runDetection);
-        }
-    });
+      if (y > 500){
+        stopScroll();
+        startScrollDown();
+      }
+      else if (y < 80){
+        stopScroll();
+        startScrollUp();
+      }
+      else if (80 <= y <= 500){
+        stopScroll();
+      };
+    }
+    
+    if (isVideo) {
+      requestAnimationFrame(runDetection);
+    }
+  });
 }
 
 function startScrollDown() {
-   scroll = setInterval(function(){ window.scrollBy(0, 20); console.log('start');}, 0.01);
+   scroll = setInterval(function(){ window.scrollBy(0, 10); console.log('start');}, 0.01);
 
 }
-function stopScrollDown() {
+function stopScroll() {
    clearInterval(scroll);
 }
 
-
+function startScrollUp() {
+  scroll = setInterval(function(){ window.scrollBy(0, -10); console.log('start');}, 0.01);
+}
 // Load the model.
 handTrack.load(modelParams).then(lmodel => {
     // detect objects in the image.
