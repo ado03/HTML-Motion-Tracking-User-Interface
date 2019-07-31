430fed63a84049fde87d5d2589008cecfcd7ec95
joycecheen <385533@bsd48.org>
2019-07-31 12:58:21 -0700

    added small changes to html and threshold

--- HTML Handtracking/track.js
+++ HTML Handtracking/track.js
@@ -34,7 +34,7 @@ const modelParams = {
     flipHorizontal: true,   // flip e.g for video
     maxNumBoxes: 1,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.8,    // confidence threshold for predictions.
+    scoreThreshold: 0.75,    // confidence threshold for predictions.
 }
 
 function startVideo() {
